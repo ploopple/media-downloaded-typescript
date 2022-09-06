c641f3c397a1d7b4739eeaa8167c520d13e3caae
plooppple <mhmda709@gmail.com>
2022-09-07 00:48:41 +0300

    init
    Signed-off-by: plooppple <mhmda709@gmail.com>

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import ytdl from "ytdl-core";
+const fs = require("fs");
+
+const app = express();
+
+// app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/api/download/:url", (req, res) => {
+  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+  ytdl(req.params.url, {
+    // @ts-ignore: Unreachable code error
+    format: "mp4",
+  }).pipe(res);
+});
+
+app.listen(4000, () => {
+  console.log("Server Works !!! At port 4000");
+});
