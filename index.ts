6ca2077ebe7ee4bf27f0238fa9d69a847b67045e
plooppple <mhmda709@gmail.com>
2022-09-08 20:51:46 +0300

    ayob
    Signed-off-by: plooppple <mhmda709@gmail.com>

--- index.ts
+++ index.ts
@@ -1,26 +1,22 @@
 import express from "express";
 import cors from "cors";
 import ytdl from "ytdl-core";
-const fs = require("fs");
 
 const app = express();
 const port = 3333;
 
-// app.use(express.json());
+// app.use(express.json())
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname.replace("dist", "") + "/index.html");
+  // res.send("d");
+});
 app.get("/api/download/:url", (req, res) => {
   res.header("Content-Disposition", 'attachment; filename="video.mp4"');
   ytdl(req.params.url, {
     // @ts-ignore: Unreachable code error
     format: "mp4",
   }).pipe(res);
-});
-
-app.get("/", (req, res) => {
-  res.send("hi");
 });
 
 app.listen(port, () => {
